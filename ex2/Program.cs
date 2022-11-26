38108311576bfbce0c85249001a027e02fc5cddb
Горшков Игорь <Igor>
2022-11-26 19:44:12 +0300

    ex 2 done

---
+++ ex2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите 2 числа:");
+Console.Write("a = ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+    Console.WriteLine($"max = {a}");
+else
+    Console.WriteLine($"max = {b}");
