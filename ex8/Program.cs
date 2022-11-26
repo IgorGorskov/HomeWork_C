ebfbf46b6fb983683014c3f69263b979efe51429
Горшков Игорь <Igor>
2022-11-26 20:57:38 +0300

    edit ex 8

--- ex8/Program.cs
+++ ex8/Program.cs
@@ -1,7 +1,11 @@
 ﻿Console.WriteLine("Введите число");
 int n = Convert.ToInt32(Console.ReadLine());
-for (int i = 1; i < n; i++)
+for (int i = 1; i < n - 1; i++)
     if (i % 2 == 0)
+    {
         Console.Write($"{i}, ");
-if (n % 2 == 0)
+    }
+if ((n - 1) % 2 == 0)
+    Console.Write(n - 1);
+else
     Console.Write(n);
