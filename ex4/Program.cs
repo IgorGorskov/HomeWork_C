c367119e02adfbfd1f329c53cb39d5956f318a8c
Горшков Игорь <Igor>
2022-11-26 19:54:18 +0300

    ex2 done

---
+++ ex4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите 3 числа");
+
+Console.Write("a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("c = ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max;
+
+if (a > b)
+    max = a;
+else
+    max = b;
+if (max > c)
+    Console.WriteLine($"max = {max}");
+else
+    Console.WriteLine($"max = {c}");
