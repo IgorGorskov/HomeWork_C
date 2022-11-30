ff7e9103d356befefe51811539572edadbcf8496
Горшков Игорь <Igor>
2022-11-30 20:43:01 +0300

    done ex_dop

---
+++ ex_dop/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите последовательность");
+
+int n = Convert.ToInt32(Console.ReadLine());
+int max = n;
+int max2 = n;
+
+while (n != 0)
+{
+    n = Convert.ToInt32(Console.ReadLine());
+    if (n > max)
+    {
+        max2 = max;
+        max = n;
+    }
+    else if (n > max2)
+        max2 = n;
+}
+
+Console.WriteLine($"max2 = {max2}");
