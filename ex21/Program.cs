db73d9b074182bfdd2f13528f2be6a4b0c94a37b
Горшков Игорь <Igor>
2022-12-04 19:25:58 +0300

    ex21 done

---
+++ ex21/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите x1");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите y1");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите z1");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите x2");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите y2");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите z2");
+double z2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(Math.Sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2) + (z1 - z2)*(z1 - z2)));
