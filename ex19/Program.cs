586a58ec5af57e167ec0cbaae19ee9b702befbc6
Горшков Игорь <Igor>
2022-12-04 19:51:09 +0300

    ex19 done

---
+++ ex19/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int ch1 = n / 10000;
+int ch2 = (n - ch1 * 10000) / 1000;
+if (n % 100 == ch1 + ch2 * 10)
+    Console.WriteLine("да");
+else
+    Console.WriteLine("нет");
