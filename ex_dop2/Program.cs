7058490e09e865a431585758be7cdb4f1e91e876
Горшков Игорь <Igor>
2022-12-04 20:34:21 +0300

    edit dop_2

--- ex_dop2/Program.cs
+++ ex_dop2/Program.cs
@@ -1,9 +1,13 @@
 ﻿Console.WriteLine("Введите число кустов");
 int n = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число ягод на каждом кусте");
+
 int cherry1 = Convert.ToInt32(Console.ReadLine());
 int cherry2 = Convert.ToInt32(Console.ReadLine());
 int cherry3 = Convert.ToInt32(Console.ReadLine());
+
+int cherry0 = cherry1;
+
 int MaxSum = cherry1 + cherry2 + cherry3;
 for (int i = 3; i < n; i++){
     cherry1 = cherry2;
@@ -12,4 +16,6 @@ for (int i = 3; i < n; i++){
     if (cherry1 + cherry2 + cherry3 > MaxSum)
         MaxSum = cherry1 + cherry2 + cherry3;
 }
+if (cherry0 + cherry2 + cherry3 > MaxSum)
+        MaxSum = cherry0 + cherry2 + cherry3;
 Console.WriteLine(MaxSum);
