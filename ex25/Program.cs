a5563454d3c593526fc569f7629600fde58be167
Горшков Игорь <Igor>
2022-12-07 23:04:54 +0300

    ex25 done

---
+++ ex25/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число А");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int B = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+while(B != 0){
+    result = result * A;
+    B--;
+}
+Console.WriteLine(result);
