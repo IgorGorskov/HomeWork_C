35ea31f4e0376cfed0874555dd10f149dacc2b9f
Горшков Игорь <Igor>
2022-12-08 20:46:50 +0300

    ex34 done

---
+++ ex_34/Program.cs
@@ -0,0 +1,18 @@
+﻿void input_random(int[] array){
+    for (int i = 0; i < array.Length; i++){
+        array[i] = new Random().Next(100,1000);
+    }
+}
+
+
+Console.Write("Массив: ");
+int[] array = new int[4];
+input_random(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+
+int counter = 0;
+for (int i = 0; i < array.Length; i++){
+    if (array[i] % 2 == 0)
+        counter++;
+}
+Console.WriteLine($"Количество четных в массиве: {counter}");
