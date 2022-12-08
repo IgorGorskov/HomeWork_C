08be27071a7ed536725f6bd3fab664e9ad6c3b27
Горшков Игорь <Igor>
2022-12-08 22:23:32 +0300

    dop_5 done

---
+++ dop_5/Program.cs
@@ -0,0 +1,18 @@
+﻿// Гипотеза Гольдбаха
+
+bool is_simple(int n){
+    for (int i = 2; i < n; i++){
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 3; i < n; i++){
+    if(is_simple(i) && is_simple(n - i)){
+        Console.Write($"{i}, {n - i}");
+        break;
+    }
+}
