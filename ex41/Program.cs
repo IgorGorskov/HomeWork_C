63b0ff5dd3d104287f4e6447fe8e161937db8a99
Горшков Игорь <Igor>
2022-12-15 08:00:56 +0300

    ex41 done

---
+++ ex41/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите количество чисел");
+int m = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int n = 0;
+for (int i = 0; i < m; i++){
+    Console.WriteLine("Введите число");
+    n = Convert.ToInt32(Console.ReadLine());
+    if(n > 0){
+        count++;
+    }
+}
+Console.WriteLine(count);
