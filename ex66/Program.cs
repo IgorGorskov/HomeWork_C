6447b8537ec4fc35bcb07a56a88bacfa45fb3868
Горшков Игорь <Igor>
2022-12-28 20:31:53 +0300

    ex66 done

---
+++ ex66/Program.cs
@@ -0,0 +1,11 @@
+﻿int sum(int m, int n){
+    if (m + 1 == n)
+        return m + n;
+    return sum(m + 1, n) + m;
+}
+Console.WriteLine("Введите m");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите n");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(sum(m, n));
